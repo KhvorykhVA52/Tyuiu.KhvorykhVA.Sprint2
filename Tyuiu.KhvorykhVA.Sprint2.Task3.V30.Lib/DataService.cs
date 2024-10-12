3a29cbc6698a5551b41a52adb2bafa32333674d6
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-10-12 13:05:45 +0500

    Task3

--- Tyuiu.KhvorykhVA.Sprint2.Task3.V30.Lib/DataService.cs
+++ Tyuiu.KhvorykhVA.Sprint2.Task3.V30.Lib/DataService.cs
@@ -9,22 +9,27 @@ namespace Tyuiu.KhvorykhVA.Sprint2.Task3.V30.Lib
             if (x > 1)
             {
                 y = Math.Pow((x + 1) / (x - 1), x);
+                Console.WriteLine($"Using case: x > 1, y = {y}");
             }
             else if (x == 0)
             {
                 y = Math.Pow(x, 2) - Math.Cos(Math.Pow(x, 2)) - 3;
+                Console.WriteLine($"Using case: x == 0, y = {y}");
             }
             else if (x > -34 && x < 2)
             {
                 y = (1 / x) - Math.Sin(Math.Pow(x, 2)) - 2;
+                Console.WriteLine($"Using case: -34 < x < 2, y = {y}");
             }
             else if (x == -34)
             {
                 y = 1 + (1 / x);
+                Console.WriteLine($"Using case: x == -34, y = {y}");
             }
             else if (x <= -34)
             {
                 y = x + 10 * x - (4.0 / 3.0);
+                Console.WriteLine($"Using case: x <= -34, y = {y}");
             }
             else
             {
